75ca67f620ddd6de99ee25d5ddf1ccd6917584eb
Patrice Mandin <patmandin@gmail.com>
2005-09-16 19:18:32 +0200

    Import gcc-cpp-3.3.6-mint-1.diff.gz
    Source:
    http://patrice.mandin.pagesperso-orange.fr/en/howto-gcc-cpp.html

--- libstdc++-v3/config/os/mint/ctype_noninline.h
+++ libstdc++-v3/config/os/mint/ctype_noninline.h
@@ -1,6 +1,7 @@
 // Locale support -*- C++ -*-
 
-// Copyright (C) 1997-1999, 2000 Free Software Foundation, Inc.
+// Copyright (C) 1997, 1998, 1999, 2000, 2001, 2002
+//  Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -32,16 +33,23 @@
 //
   
 // Information as gleaned from /usr/include/ctype.h
-  
+
   const ctype_base::mask*
   ctype<char>::classic_table() throw()
   { return 0; }
 
+  ctype<char>::ctype(__c_locale, const mask* __table, bool __del, 
+		     size_t __refs) 
+  : __ctype_abstract_base<char>(__refs), _M_del(__table != 0 && __del), 
+  _M_toupper(NULL), _M_tolower(NULL), 
+  _M_table(__table ? __table : classic_table()) 
+  { }
+
   ctype<char>::ctype(const mask* __table, bool __del, size_t __refs) 
-    : __ctype_abstract_base<char>(__refs), _M_del(__table != 0 && __del), 
-      _M_toupper(NULL), _M_tolower(NULL),
-      _M_table(__table ? __table : classic_table()) 
-    { }
+  : __ctype_abstract_base<char>(__refs), _M_del(__table != 0 && __del), 
+  _M_toupper(NULL), _M_tolower(NULL), 
+  _M_table(__table ? __table : classic_table())
+  { }
 
   char
   ctype<char>::do_toupper(char __c) const
