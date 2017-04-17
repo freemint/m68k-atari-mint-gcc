dea8924c5f97ee40919d58136ea49bf4895248d7
Vincent Rivière <vincent.riviere@freesbee.fr>
2017-04-17 17:51:07 +0200

    Update version to 20170417

--- gcc/version.c
+++ gcc/version.c
@@ -32,5 +32,5 @@ const char bug_report_url[] = BUGURL;
    BASEVER, DATESTAMP, DEVPHASE, and REVISION are defined by the
    Makefile.  */
 
-const char version_string[] = BASEVER DATESTAMP DEVPHASE REVISION " (MiNT 20130415)";
+const char version_string[] = BASEVER DATESTAMP DEVPHASE REVISION " (MiNT 20170417)";
 const char pkgversion_string[] = PKGVERSION;
