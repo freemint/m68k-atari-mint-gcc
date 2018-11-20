0bec9786df1ac39a266513d14415024bc74defa6
Miro Kropacek <miro.kropacek@gmail.com>
2018-11-20 10:05:11 +0100

    Re-introduce custom version string

--- gcc/version.c
+++ gcc/version.c
@@ -31,5 +31,5 @@ const char bug_report_url[] = BUGURL;
    BASEVER, DATESTAMP, DEVPHASE, and REVISION are defined by the
    Makefile.  */
 
-const char version_string[] = BASEVER DATESTAMP DEVPHASE REVISION;
+const char version_string[] = BASEVER DATESTAMP DEVPHASE REVISION " (MiNT 20181120)";
 const char pkgversion_string[] = PKGVERSION;
